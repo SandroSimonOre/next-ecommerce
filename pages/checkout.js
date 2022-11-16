932d5df8633b012ee30933123c55518e57f6d26b
SANDRO SIMON <sandrosimonore@gmail.com>
2022-11-16 17:55:30 -0500

    Checkout feature in progress

--- pages/checkout.js
+++ pages/checkout.js
@@ -9,19 +9,40 @@ import { setPaymentMethod } from '../features/cart/cartSlice';
 
 export default function CheckoutPage() {
 
-    const [activeStep, setActiveStep] = useState(1);
+    
+    const [activeStep, setActiveStep] = useState(3);
+    const [selectedPaymentMethod, setSelectedPaymentMethod] = useState(0);
+    const handleChangingStep = (step)=> {
+        setActiveStep(step)
+    }
+
+    const handleSubmitIdentificationForm = (e) => {
+        //e.preventDefault();
+        setActiveStep(2)
+    };
+
+    const handleSubmitShipping = (e) => {
+        setActiveStep(3);
+    };
     
   return (
     <Layout title="Payment Method">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-1">
             <div className=''>
-                <div className='flex justify-between border-2 border-gray-100'>
+                <h2>Summary</h2>
+                <h2>Your order detail</h2>
+            </div>
+            <div className=''>
+                <div className='flex justify-between border-2 border-red-800'>
                     <h3>Address</h3>
-                    <p>Edit</p>
+                    {
+                        activeStep !== 1 &&
+                            <p onClick={()=> handleChangingStep(1)}>Edit</p>
+                    }
                 </div>
                 { 
                     activeStep === 1 &&
-                        <div>
+                        <div className='h-60 border-2 border-red-800'>
                             <form action="">
                                 <input type="submit" value="Continue" />
                             </form>
@@ -29,13 +50,17 @@ export default function CheckoutPage() {
 
                 }
                 
-                <div className='flex justify-between border-2 border-gray-100'>
+                <div className='flex justify-between border-2 border-blue-800'>
                     <h3>Shipping</h3>
-                    <p>Edit</p>
+                    {
+                        activeStep !== 2 &&
+                            <p onClick={()=> handleChangingStep(2)}>Edit</p>
+                    }
+                    
                 </div>
                 { 
                     activeStep === 2 &&
-                        <div>
+                        <div className='h-60 border-2 border-blue-800'>
                             <form action="">
                                 <input type="submit" value="Continue" />
                             </form>
@@ -43,44 +68,67 @@ export default function CheckoutPage() {
 
                 }
                 
-                <div className='flex justify-between border-2 border-gray-100'>
+                <div className='flex justify-between border-2 border-green-800'>
                     <h3>Payment</h3>
-                    <p>Edit</p>
+                    {
+                        activeStep !== 3 &&
+                            <p onClick={()=> handleChangingStep(3)}>Edit</p>    
+                    }
                 </div>
-                <div>
+                
                 {
                     activeStep === 3 &&
-                    <>
-                       <div>
-                        <h2>CashOnDelivery</h2>
-                        <form action="">
-                            <input type="submit" value="Continue" />
-                        </form>
-                        </div>
-                        <div>
-                            <h2>Paypal</h2>
-                            <form action="">
-                                <input type="submit" value="Continue" />
-                            </form>    
-                        </div>
-                        <div>
-                            <h2>Stripe</h2>  
-                            <form action="">
-                                <input type="submit" value="Finish" />
-                            </form>    
-                        </div>
-                    </>
+                    <div className='h-60 border-2 border-green-800'>
+                        
+                        <div 
+                            className='h-30 bg-blue-400'
+                            onClick={()=>setSelectedPaymentMethod(1)}
+                        >Paypal</div>
+                        {
+                            selectedPaymentMethod === 1 &&
+                                <div>
+                                    <h2>Paypal</h2>
+                                    <form action="">
+                                        <input type="submit" value="Continue" />
+                                    </form>    
+                                </div>
+                                
+                        }
+                        
+                        <div
+                            className='h-30 bg-green-400' 
+                            onClick={()=>setSelectedPaymentMethod(2)}
+                        >Stripe</div>
+                        {   
+                            selectedPaymentMethod === 2 &&
+                            <div>
+                                <h2>Stripe</h2>  
+                                <form action="">
+                                    <input type="submit" value="Finish" />
+                                </form>    
+                            </div>
+                        }
+
+                        <div
+                            className='h-30 bg-yellow-400' 
+                            onClick={()=>setSelectedPaymentMethod(3)}
+                        >Contraentrega</div>
+                        {   selectedPaymentMethod === 3 &&
+                            
+                            <div>
+                                <h2>CashOnDelivery</h2>
+                                <form action="">
+                                    <input type="submit" value="Continue" />
+                                </form>
+                            </div>
+                        }
+                        
+                    </div>
                 }
                     
-                </div>
+                
             </div>
 
-            <div>
-                <h2>Summary</h2>
-                <form action="">
-                    <input type="submit" />
-                </form>
-            </div>
         </div>
     </Layout>
   );
